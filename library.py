2cccdc3904eea1e5e85e10c01c7339bc47b63f43
BleakHeart <cosmin.marin19@gmail.com>
2021-10-22 22:26:00 +0200

    New functions

--- library.py
+++ library.py
@@ -1,26 +1,23 @@
 import numpy as np 
 
 
-def entropy(x):
+def entropy(pX):
     """
     This function calculates the entropy given an input dataset
 
     Args:
-        x ([numpy.ndarray]): Input data
+        pX ([numpy.ndarray]): Input probability mass distribution
 
     Returns:
         numpy.float64: It is the computed entropy 
     """
 
     'I find the unique values of x in order to get the mass function of x'
-    value, counts = np.unique(x, return_counts=True) 
-
-    del value # I don't need this information to compute the entropy
-    p = counts / counts.sum() # calculate the probability mass function
-    return np.sum(-p * np.log2(p)) # Return the entropy computed as defined by Shannon 
+    
+    return np.sum(-pX * np.log2(pX)) # Return the entropy computed as defined by Shannon 
 
 
-def entropy_joint(x, numBins=30):
+def entropy_joint(pXY):
     """[summary]
 
     Args:
@@ -31,6 +28,30 @@ def entropy_joint(x, numBins=30):
         [type]: [description]
     """
 
-    p, _ = np.histogramdd(x, bins=30)
-    p /= p.sum()
-    return np.sum(-p * np.log2(p))
+    return -np.sum(pXY * np.log2(pXY))
+
+
+def conditional_entropy(pXY, pX):
+    """[summary]
+
+    Args:
+        pXY ([type]): [description]
+        pX ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    pY_givenX = pXY / pX
+    return - np.sum(pXY * np.log2(pY_givenX)) 
+
+
+def mutual_information(pXY, pX, pY):
+    """[summary]
+
+    Args:
+        pXY ([type]): [description]
+        pX ([type]): [description]
+        pY ([type]): [description]
+    """
+    
+    return entropy(pX) + entropy(pY) - entropy_joint(pXY)
