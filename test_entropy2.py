44c49ffc380d4bae875a5ba3a2bf12877d0313fc
BleakHeart <cosmin.marin19@gmail.com>
2021-10-25 09:25:26 +0200

    Added some comments

--- test_entropy2.py
+++ test_entropy2.py
@@ -2,14 +2,25 @@ import numpy as np
 from library import entropy
 import matplotlib.pyplot as plt
 
-P = np.linspace(0, 1, 100)
-S = []
+# Defining the Probabilities and the Entropy list
+P = np.linspace(0, 1, 100)  # p0 array probabilities
+S = []                      # Entropy empty list
+
+
+# Computing the entropy for the p0 values contained in P array
 for p0 in P:
-    data = np.random.choice([0, 1], p=[p0, 1 - p0], size=100000)
+    # Creating a binary random array with p0 and 1-p0 probabilities of 280000 shape.
+    data = np.random.choice([0, 1], p=[p0, 1 - p0], size=280000) 
+
+    # computing the data probabilities
     values, counts = np.unique(data, return_counts=True)
     pdf = counts / counts.sum()
+
+    # saving the entropy value for this set of data
     S.append(entropy(pdf))
 
+
+# Plotting the results
 plt.plot(P, S)
 plt.xlabel(r'$p_0$')
 plt.ylabel('Entropy (S)')
