b72f08c56f8df010357089d0e55cf73319d75845
BleakHeart <cosmin.marin19@gmail.com>
2021-12-13 17:05:35 +0100

    implemented the Naive Bayes

---
+++ library/classifiers.py
@@ -0,0 +1,11 @@
+from _typeshed import Self
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+from statsmodels.nonparametric.kernel_density import KDEMultivariate, KDEMultivariateConditional
+import pandas as pd
+
+
+class Bayes_Classifier():
+    def __init__(self) -> None:
+        self.
