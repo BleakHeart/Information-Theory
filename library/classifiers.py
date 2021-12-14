c61ec1f618d56839cca35a4268eb9ced1708ff31
BleakHeart <cosmin.marin19@gmail.com>
2021-12-14 15:03:43 +0100

    Implemented Naive Bayes classifier

--- library/classifiers.py
+++ library/classifiers.py
@@ -25,33 +25,45 @@ class Bayes_Classifier:
     def evaluate(self, X_test):
         n = X_test.shape[0]
 
-        PXc = np.array([self.kde_Xc(X_test, [i] * n) for i in range(3)])
+        PXc = np.array([self.kde_Xc(X_test, [i] * n) for i in range(self.n_classes)])
         PcX = ((PXc.T * self.Prior).T / self.kde_X(X_test)).T
 
         return PcX
 
 
-def Naive_Bayes_classifier(X, y, dep_type, indep_type):
-    n_classes = len(np.unique(y))
-    n_features = X.shape[1]
+class NB_classifier:
+    def __init__(self, X, y, dep_type, indep_type, bw=None) -> None:
+        self.n_classes = np.unique(y).size
+        self.n_features = X.shape[1]
 
-    kde_Xc = []
+        self.Prior = np.unique(y, return_counts=True)[1] / y.size
 
-    for i in range(n_features):
-        kde_Xc.append(KDEMultivariateConditional(endog=X[:, i], exog=y, 
-                                                 dep_type=dep_type, 
-                                                 indep_type=indep_type, 
-                                                 bw='normal_reference').pdf)
+        if bw == None:
+            bw = 'normal_reference'
+        else:
+            bw = bw
+
+        self.kde_Xc = []
+
+        for i in range(self.n_features):
+            self.kde_Xc.append(KDEMultivariateConditional(endog=X[:, i], exog=y, 
+                                                     dep_type=dep_type, 
+                                                     indep_type=indep_type, 
+                                                     bw='normal_reference').pdf)
         
-    kde_X = KDEMultivariate(X, var_type='cccc').pdf
+        self.kde_X = KDEMultivariate(X, var_type='cccc').pdf
 
-    Prior = np.unique(y, return_counts=True)[1] / y.size
-
-    c = np.zeros(n_classes)
-    for i in range(n_classes):
-        tmp = 1
-        for j in range(n_features):
-            tmp *= kde_Xc[j]([X[0, j]], [i])
-        c[i] = tmp * Prior[i] / kde_X(X[0, :])
-
-    return c
+        self.Prior = np.unique(y, return_counts=True)[1] / y.size
+    
+    def evaluate(self, X):
+        n_rows = X.shape[0]
+        c = np.zeros((n_rows, self.n_classes))
+        
+        for d in range(n_rows):
+            for i in range(self.n_classes):
+                tmp = 1
+                for j in range(self.n_features):
+                    tmp *= self.kde_Xc[j]([X[d, j]], [i])
+                
+                c[d, i] = tmp * self.Prior[i] / self.kde_X(X[d, :])
+        return c
