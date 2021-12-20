4d931f4543c9b90eedc370290ccfeb9fd0b3bc56
BleakHeart <cosmin.marin19@gmail.com>
2021-12-20 17:04:47 +0100

    Added plottings for latex

--- test_entropy2.py
+++ test_entropy2.py
@@ -3,29 +3,33 @@ from library.information_discrete import E
 import matplotlib.pyplot as plt
 from library.information_continuos import diff_E
 from scipy.stats import norm
-import seaborn as sns
+import matplotlib as mpl
+
+fig_width_pt = 390.0                # Get this from LaTeX using \the\columnwidth
+inches_per_pt = 1.0/72.27               # Convert pt to inches
+golden_mean = (np.sqrt(5)-1.0)/2.0         # Aesthetic ratio
+fig_width = fig_width_pt*inches_per_pt  # width in inches
+fig_height =fig_width*golden_mean       # height in inches
+fig_size = [fig_width,fig_height]
+params = {'backend': 'ps',
+          'axes.labelsize': 10,
+          'legend.fontsize': 10,
+          'xtick.labelsize': 8,
+          'ytick.labelsize': 8,
+          'figure.figsize': fig_size,  
+          'axes.axisbelow': True}
+
+mpl.rcParams.update(params)
+
 
 
 P = np.linspace(0, 1, 100)  # p0 array probabilities
-S_estimated = []                      # Entropy empty list
+
 
 P_real = np.vstack([P, 1 - P]).T
 
-# Computing the entropy for the p0 values contained in P array
-for p in P_real:
-    # Creating a binary random array with p0 and 1-p0 probabilities of 280000 shape.
-    data = np.random.choice([0, 1], p=p, size=20000) 
-
-    # computing the data probabilities
-    values, counts = np.unique(data, return_counts=True)
-    pdf = counts / counts.sum()
-
-    # saving the entropy value for this set of data
-    S_estimated.append(E(pdf))
-
 # Computing the real Entropy to see the differences
 S_real = [E(p) for p in P_real]
-
 
 # Plotting the results
 fig, axs = plt.subplots(1, 2, figsize=(12, 9))
@@ -34,10 +38,31 @@ axs[0].plot(P, S_real, 'k')
 axs[0].set_xlabel(r'$p_0$')
 axs[0].set_ylabel('Entropy (S)')
 axs[0].set_title('Results from the real pmf')
+axs[0].grid()
 
-axs[1].plot(P, S_estimated, 'k')
+# Computing the estimated entropy for different numbers of generated samples 
+for s in [100, 1000, 10000, 20000]:
+    S_estimated = []                      # Entropy empty list
+
+    # Computing the entropy for the p0 values contained in P array
+    for p in P_real:
+        # Creating a binary random array with p0 and 1-p0 probabilities of 280000 shape.
+        data = np.random.choice([0, 1], p=p, size=s) 
+
+        # computing the data probabilities
+        values, counts = np.unique(data, return_counts=True)
+        pdf = counts / counts.sum()
+
+        # saving the entropy value for this set of data
+        S_estimated.append(E(pdf))
+    
+    axs[1].plot(P, S_estimated, label=f'samples={s}')
+
+
 axs[1].set_xlabel(r'$p_0$')
-axs[1].set_ylabel('Entropy (S)')
 axs[1].set_title('Results from the estimated pmf')
-
-plt.show()
+axs[1].grid()
+fig.tight_layout()
+plt.legend()
+plt.savefig('discrete_Entropy.png', dpi=600, transparent=False)
+#plt.show()
